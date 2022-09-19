0d7edd23c832b3b9f6a84166818fd8993d6e3b50
Hank Liu <min9han9@gmail.com>
2022-09-19 10:58:59 +0800

    spring boot project

---
+++ src/test/java/com/hank/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hank.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
